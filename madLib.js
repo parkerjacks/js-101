8f71792152d10b307625602743983c22210aaad2
Park Jack <parker.jackson.hhs@gmail.com>
2020-04-20 13:14:49 -0400

    created a tip calculator file

---
+++ madLib.js
@@ -0,0 +1,6 @@
+function madlib(name,favoriteSubject){ 
+    console.log(`${name}'s favorite subject in school is ${favoriteSubject}.`); 
+} 
+
+madlib("Parker","History")
+madlib("Matthew","Math")
---
+++ tipCalculator.js
@@ -0,0 +1,21 @@
+
+
+
+function tipCalculator(total,service){ 
+    if (service === "Good"){ 
+        let tipAmount = total * .20;
+        console.log(tipAmount);
+
+    } else if (service === "Fair"){  
+        let tipAmount = total * .15; 
+        console.log(tipAmount);
+
+    } if (service ==="Bad") { 
+        let tipAmount = total * .10; 
+        console.log(tipAmount); }
+
+} 
+
+tipCalculator(10,"Fair")
+tipCalculator(10,"Good") 
+tipCalculator(10,"Bad")
